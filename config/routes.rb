434f87c213f485a261f1258ce6e04e14be87c7eb
carolniuyan <yanxinyi@grinnell.edu>
2020-04-02 16:28:02 -0500

    Fixed navigation bar & footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/about'
+  get '/about', to: 'static_pages#about'
+  get '/help', to: 'static_pages#help'
+  get '/contact', to: 'static_pages#contact'
 end
