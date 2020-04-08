3d3514e234c039cd4bb91faae2a6739287321715
root <root@DESKTOP-8JURB5F.localdomain>
2020-04-08 15:42:01 -0700

    Installed&Set up activeadmin

---
+++ db/migrate/20200408215507_drop_table.rb
@@ -0,0 +1,2 @@
+class DropTable < ActiveRecord::Migration[6.0]
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -54,3 +54,4 @@ items.each do |thing|
 end
 
 
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
