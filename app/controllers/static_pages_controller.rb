4c0e3a0ea97f8e34a7f63d7fd736d25ed0d9aa28
carolniuyan <yanxinyi@grinnell.edu>
2020-04-08 22:24:25 -0500

    home page needed food/non-food items displayed

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,8 @@ class StaticPagesController < ApplicationController
   def home
     @food_items = Item.where(item_type: 'Food').order('name ASC')
     @non_food_items = Item.where(item_type: 'Non-Food').order('name ASC')
-    @needed_food_items = Item.where('priority > 0').order('priority DESC')
+    @needed_food_items = @food_items.where("priority > 0").order('priority DESC')
+    @needed_non_food_items = @non_food_items.where('priority > 0').order('priority DESC')
   end
 
   def about
