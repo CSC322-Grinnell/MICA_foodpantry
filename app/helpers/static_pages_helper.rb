d0fa79440d31b8fa24cbaea4cdc72bc64479b9e0
Sam Eagen <sve9milo@gmail.com>
2021-05-10 15:32:09 -0500

    Responsive design now in place. Added key. Addec color-coding. Fixed some bugs

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,13 @@
 module StaticPagesHelper
+  def color_picker(item)
+    if item.priority <= 25
+      return "display:none"
+    elsif item.priority <= 50
+      return  "background-color:#8DAF2D"
+    elsif item.priority <= 75
+      return "background-color:#F2C83E"
+    elsif item.priority <= 100 
+      return "background-color:#b81b09"
+    end
+  end
 end
